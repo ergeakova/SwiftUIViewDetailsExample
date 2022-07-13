1fd2989600fa8596027a0c6b29363fc74abaa669
ergeakova <ergeak@gmail.com>
2022-07-13 23:42:02 +0300

    Initial Commit

---
+++ ViewDetailsExample/ViewDetailsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewDetailsExampleApp.swift
+//  ViewDetailsExample
+//
+//  Created by Erge Gevher Akova on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewDetailsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
